0aa6374c3f6201a9f09dd4930ba2552986df11d9
4rn0h <okoth.4rn0h@gmail.com>
2025-07-17 19:51:18 +0300

    railway updates gunicorn

--- rmotors_backend/settings.py
+++ rmotors_backend/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 # Static files (PythonAnywhere specific)
 #STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files configuration
 STATIC_URL = '/static/'
