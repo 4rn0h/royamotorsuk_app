b12e74c1a67114e7bb8d1ce323b713f3f64f8ee6
4rn0h <okoth.4rn0h@gmail.com>
2025-07-21 11:32:05 +0300

    wsgi.py updated

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,6 @@
 import os
-import sys
+from django.core.wsgi import get_wsgi_application
 
-# Add your project directory to the Python path
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Set Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rmotors_backend.settings')
 
-# Import Django WSGI application
-from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-
-
