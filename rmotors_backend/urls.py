8a8ae7152f050a37ee735bffa87a60a285aa02dd
4rn0h <okoth.4rn0h@gmail.com>
2025-07-21 11:52:30 +0300

    main urls.py updated

--- rmotors_backend/urls.py
+++ rmotors_backend/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve as django_serve
 from enquiries.views import EnquiryCreateAPIView
 from users.views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,12 +18,21 @@ urlpatterns = [
     path('api/', include('testimonials.urls')),
     path('api/', include('chat.urls')),
     path('api/enquiries/', EnquiryCreateAPIView.as_view(), name='enquiry-create'),
+
+    #Serve static files like /assets/... before catch-all route
+    re_path(r'^assets/(?P<path>.*)$', django_serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'dist/assets'),
+    }),
+    re_path(r'^favicon.ico$', django_serve, {
+        'document_root': os.path.join(settings.BASE_DIR, 'dist'),
+    }),
 ]
 
+# Serve media in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Catch-all for React frontend, EXCLUDING admin, api, static, and media routes
+#Catch-all for React routes AFTER static/assets
 urlpatterns += [
-    re_path(r'^(?!admin/|api/|static/|media/).*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^(?!admin/|api/).*$', TemplateView.as_view(template_name='index.html')),
 ]
